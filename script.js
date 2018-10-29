a2566e81f9d8766b7c673eee56d94f52b6935569
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-10-29 16:24:16 -0400

    Updated JavaScript to include user inputs

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$.ajax({
-      url: "url",
-      success: function(data) {
-            alert(data);
-      },
-      error: function() {
-            alert("HTTP request was unsuccessful. Please try another URL.");
-      }
-});
+$("#load-url").click(function() {
+      var url = $("#url")[0].value;
+      $.ajax({
+            url: url,
+            success: function(data) {
+                  alert(data);
+            },
+            error: function() {
+                  alert("HTTP request was unsuccessful. Please try another URL.");
+            }
+      });
+})
